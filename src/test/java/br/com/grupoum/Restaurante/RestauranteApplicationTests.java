abea12f9d3a6367ee2a29e298e3196dea5fdd46e
Sysorkole <arthurfigon@gmail.com>
2022-02-15 13:12:33 -0300

    Versão Inicial Só com o modelo e os imports

---
+++ src/test/java/br/com/grupoum/Restaurante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupoum.Restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
