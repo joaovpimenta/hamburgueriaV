262e46b2155c0d50ba2225779a765fc460a26ad8
Sysorkole <arthurfigon@gmail.com>
2022-02-16 14:14:19 -0300

    Versão com Controller, Mesa Entity(pendente alguns atributos), Service e Repository. E Banco de Dados Postgres...

--- src/main/java/br/com/grupoum/Restaurante/model/entities/Cliente.java
+++ src/main/java/br/com/grupoum/Restaurante/model/entities/Cliente.java
@@ -1,4 +1,21 @@
 package br.com.grupoum.Restaurante.model.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Cliente {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
