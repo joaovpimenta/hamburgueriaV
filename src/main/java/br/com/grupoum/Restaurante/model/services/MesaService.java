0462a8f08a912fd592e80d326780e6c6879a9279
Sysorkole <arthurfigon@gmail.com>
2022-02-17 10:11:21 -0300

    Criação das Classes e Repositories

--- src/main/java/br/com/grupoum/Restaurante/model/services/MesaService.java
+++ src/main/java/br/com/grupoum/Restaurante/model/services/MesaService.java
@@ -5,6 +5,10 @@ import br.com.grupoum.Restaurante.model.repositories.MesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MesaService {
 
@@ -14,13 +18,36 @@ public class MesaService {
 
     // Validação
 
+
+    // Create/Update a Mesa
     public boolean createMesa(Mesa mesa){
-        if(mesa.getStatus()){
+        if(mesa != null){
             repository.save(mesa);
             return true;
         }
         return false;
     }
 
+    // Delete a Mesa
+    public boolean deleteMesaById(Long id){
+        Optional<Mesa> mesaToDelete = repository.findById(id);
+        if(mesaToDelete.isPresent()){
+            repository.delete(mesaToDelete.get());
+            return true;
+        }
+        return false;
+    }
+
+    // Read a Mesa
+    public Mesa findMesaByID(Long id){
+        Optional<Mesa> mesa = repository.findById(id);
+        return mesa.orElse(null);
+    }
+
+    // Read all Mesas
+    public List<Mesa> findAllMesas(){
+        List<Mesa> mesas = repository.findAll();
+        return mesas;
+    }
 
 }
