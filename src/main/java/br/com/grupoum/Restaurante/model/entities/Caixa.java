7979247200e26d62cad49e872f5d4ce229c81918
João Victor <52414666+joaovpimenta@users.noreply.github.com>
2022-02-17 10:58:26 -0300

    Atualização em erros do merge anterior

--- src/main/java/br/com/grupoum/Restaurante/model/entities/Caixa.java
+++ src/main/java/br/com/grupoum/Restaurante/model/entities/Caixa.java
@@ -1,7 +1,31 @@
 package br.com.grupoum.Restaurante.model.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 public class Caixa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_caixa")
+    private Long id;
+    @Column(name = "VALORTOTAL_CAIXA")
+    private BigDecimal valorTotal;
+
+    public Caixa() {
+
+    }
+
+    public Caixa(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
 }
--- src/main/java/br/com/grupoum/Restaurante/model/entities/Estoque.java
+++ src/main/java/br/com/grupoum/Restaurante/model/entities/Estoque.java
@@ -1,7 +1,11 @@
 package br.com.grupoum.Restaurante.model.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Entity
+
 public class Estoque {
+
 }
